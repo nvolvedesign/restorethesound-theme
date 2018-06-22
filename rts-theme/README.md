be2fe3c1e6de7640be0e311431c4b6998ea74bdd
Sammuel Nevarez <snevarez89@gmail.com>
2018-06-21 21:39:35 -0700

    first commit

---
+++ rts-theme/README.md
@@ -0,0 +1,1 @@
+# restorethesound-theme
---
+++ rts-theme/flatsome-child/audio15-custom-page.php
@@ -0,0 +1,252 @@
+<?php /* Template Name: Audio15 Custom Page */ ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Download Royalty Free Music for your Productions</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="Royalty Free Music for Youtubers, Gamers, Filmmakers, and Producers">
+		<meta name="author" content="Restore The Sound">
+		<meta name="keywords" content="royalty free music, youtubers, film makers, background, gamer, twitch, subscribers, call of duty world war II, battlefield, montage, sniper, kills, counters-strike">
+		<link rel="icon" href="https://restorethesound.com/wp-content/uploads/2016/12/cropped-RTS-LOGO-icon-600-300x300.png" sizes="32x32" />
+		<link rel="icon" href="https://restorethesound.com/wp-content/uploads/2016/12/cropped-RTS-LOGO-icon-600-300x300.png" sizes="192x192" />
+		<link rel="apple-touch-icon-precomposed" href="https://restorethesound.com/wp-content/uploads/2016/12/cropped-RTS-LOGO-icon-600-300x300.png" />
+		<meta name="msapplication-TileImage" content="https://restorethesound.com/wp-content/uploads/2016/12/cropped-RTS-LOGO-icon-600-300x300.png" />
+		<script src='https://www.google.com/recaptcha/api.js'></script> </head>
+		<script src="https://restorethesound.com/wp-content/themes/flatsome-child/lity/vendor/jquery.js"></script>
+		<script src="https://restorethesound.com/wp-content/themes/flatsome-child/lity/dist/lity.js"></script>
+		<link rel='stylesheet' id='bootstrap-audio15-css'  href='https://restorethesound.com/wp-content/themes/flatsome-child/bootstrap/css/bootstrap-audio15.css?ver=4.7' type='text/css' media='all' />
+		<link rel='stylesheet' id='lity-css-css'  href='https://restorethesound.com/wp-content/themes/flatsome-child/lity/dist/lity.css?ver=4.7' type='text/css' media='all' />
+		<link rel='stylesheet' id='bootstrap-css-css'  href='https://restorethesound.com/wp-content/themes/flatsome-child/bootstrap/css/bootstrap.min.css' type='text/css' media='all' />
+		<script src="https://restorethesound.com/wp-content/themes/flatsome-child/js/jquery.js"></script>
+		<script src="https://restorethesound.com/wp-content/themes/flatsome-child/js/bootstrap.js"></script>
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-90890000-1', 'auto');
+		  ga('send', 'pageview');
+
+		</script>
+	</head>
+<body>
+	<!--
+    <nav class="navbar navbar-color navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand a15-color-orange opensans300" href="https://restorethesound.com">
+                    <strong>RESTORE</strong>THE<strong>SOUND</strong>
+                </a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="/shop">Enter <strong>Store</strong></a>
+ 					</li>
+                </ul>
+            </div>
+        </div>
+    </nav>-->
+    <!-- 100% row BEGIN-->
+    <div class="top-row">
+    	<div class="top-row-box">
+			<div class="box-header-left">
+				<img src="https://restorethesound.com/wp-content/uploads/2015/12/3-collections.png" alt="3 music collections digital download" />
+			</div>
+			<div class="box-header-right a15-color-white opensans300">
+				<span class="product-msg-header a15-color-white text-shadow"><strong>3 COLLECTIONS:</strong><br>Enjoy Royalty Free Music for your productions! 
+				</span>
+				<a href="https://restorethesound.com/checkout/?add-to-cart=62">
+				<div class="a15-button opensans300"><strong>DOWNLOAD</strong></div></a>
+				<a href="https://restorethesound.com/shop"><span class="product-msg-header a15-color-white text-shadow"><u>Enter Store</u></span></a>
+			</div>
+		</div>
+	</div>
+ 	<div class="audio15-container center-block">
+	<!-- 100% row END-->
+<!-- lightbox videos start -->
+		<div class="row">
+		<h1 class="main-title a15-color-blue">Download Royalty Free Music for your productions!</h1>
+		<div class="paragraph-message">
+				<img class="youtube-cartoon clear-img" src="https://restorethesound.com/wp-content/uploads/2016/07/youtubers-cartoon.jpg" alt="youtube cartoon of people using audio 15" width="300" />
+				<p class="opensans300 a15-color-black">
+Audio15 includes <strong>3 music collections</strong> designed for youtubers, filmmakers, bloggers, producers, and marketing teams.<br /><br />These <strong>15 tracks</strong> will leave listeners at the edge of their seat as they experience your production become alive.
+					</p>
+			</div>
+		<hr>
+		<div class="opensans300 sub-title a15-color-blue text-center">Preview all 3 collections below!</div>	
+		</div>
+		<div class="row">
+			<a href="//https://www.youtube.com/watch?v=DW2H6Jt0Wug&rel=0&showinfo=0" data-lity>
+				<div class="col-md-4 col-sm-4 col-xs-4 category-box-1">
+					<div class="category-a15-button opensans300">
+	  					<span class="glyphicon glyphicon-rts glyphicon-play-circle a15-color-orange play-icon-size" aria-hidden="true"></span><br />Inspiration
+	  				</div>
+	  			</div>
+	  		</a>
+			<a href="//www.youtube.com/watch?v=yE7NMVvhUho&rel=0&showinfo=0" data-lity>
+				<div class="col-md-4 col-sm-4 col-xs-4 category-box-2">
+					<div class="category-a15-button opensans300">
+  						<span class="glyphicon glyphicon-rts glyphicon-play-circle a15-color-orange play-icon-size" aria-hidden="true"></span><br />Cinema
+  					</div>
+				</div>
+			</a>
+			<a href="//https://www.youtube.com/watch?v=O-HHnxyFggU&rel=0&showinfo=0" data-lity>
+				<div class="col-md-4 col-sm-4 col-xs-4 category-box-3">
+					<div class="category-a15-button opensans300">
+  						<span class="glyphicon glyphicon-rts glyphicon-play-circle a15-color-orange play-icon-size" aria-hidden="true"></span><br />Filler
+  					</div>
+				</div>
+			</a>
+		</div>
+		<!-- lightbox videos end -->
+		<hr>
+		<div class="row">
+			<h2 class="opensans300 main-title a15-color-blue">Successes</h2>
+		</div>
+		<div class="row">
+			<!-- right success container -->
+			<div class="success-container-left">
+				<a href="//https://www.youtube.com/watch?v=BGTuPmZ21r0&rel=0&showinfo=0" data-lity>
+					<div class="success-video-left"></div>
+				</a>
+				<div class="paragraph-message">
+					<p class="opensans300 a15-color-black success-video-message">
+						Funker Tactical uses a variety of songs from the cinema collection for their youtube videos as a way of making their productions fun, educational and suspenseful.
+					</p>
+				</div>
+			</div>
+			<!-- right success container -->
+			<div class="success-container-right">
+				<a href="//https://www.youtube.com/watch?v=rvq1b8nKkyg&rel=0&showinfo=0" data-lity>
+					<div class="success-video-right"></div>
+				</a>
+				<div class="paragraph-message">
+					<p class="opensans300 a15-color-black success-video-message">
+						Each Sunday, CenterPoint Church in Murrieta California uses the track Man on the hill from the inspiration collection as their main theme for their weekly video.
+					</p>
+				</div>
+			</div>
+		</div>
+		<hr>
+		<div class="row">
+			<h2 class="margin-top opensans300 main-title a15-color-blue">Why Music Matters</h2>
+		</div>
+		<div class="row">
+			<div class="paragraph-message">
+				<p class="opensans300 a15-color-black">
+					Both movies and trailers include powerful music to drive the visuals. With every presentation, it’s ideal to include a song that speaks to a particular emotion to capture your audience. With Audio15, you will have an opportunity to establish recognition while keeping your viewers entertained.
+				</p>
+				<br />
+				<p>
+				<div class="better-xp">
+				<img class="center-block" alt="music production audio" src="https://restorethesound.com/wp-content/uploads/2016/12/a-better-XP-1.png" />
+				</div>
+				</p>
+					<br/>
+				<p class="opensans300 a15-color-black text-center">Bonus tracks are included! <i>All songs are available in mp3 format.</i></p>
+			</div>
+		</div>
+		<div class="row">
+			<a href="https://restorethesound.com/checkout/?add-to-cart=62"><div class="a15-button-center opensans300"><strong>DOWNLOAD</strong></div></a>
+		</div>
+		<hr>
+		<!--<div class="row">
+			<div class="center-block text-center youtubers-cartoon">
+				<img src="https://restorethesound.com/wp-content/uploads/2016/07/youtubers-cartoon.jpg" />
+			</div>
+		</div>-->
+		<div class="row">
+		<h2 class="opensans300 main-title a15-color-blue">This Is What You Get</h2>
+			<div class="opensans sub-title a15-color-orange">Inspiration - 5 Tracks</div>
+		</div>
+		<div class="row">
+			<div class="paragraph-message">
+				<p class="opensans300 a15-color-black">
+					<img class="collection-img clear-img" src="<?php echo get_stylesheet_directory_uri(); ?>/img/inspiration-package.png" alt="inspiration music collection digital download" width="300" />Build inspiration, hope, and happiness. If you are promoting something special, adding these tracks will touch the hearts of your audience while creating a strpmg connection with your story.
+				</p>
+			</div>
+			<br>
+			<ul class="list-group">
+			  <li class="list-group-item"><span class="glyphicon glyphicon-rts glyphicon-headphones a15-color-orange" aria-hidden="true"></span> As She Paints the Sky.mp3</li>
+			  <li class="list-group-item"><span class="glyphicon glyphicon-rts glyphicon-headphones a15-color-orange" aria-hidden="true"></span> Lets hold Hands.mp3</li>
+			  <li class="list-group-item"><span class="glyphicon glyphicon-rts glyphicon-headphones a15-color-orange" aria-hidden="true"></span> Man on the Hill.mp3</li>
+			  <li class="list-group-item"><span class="glyphicon glyphicon-rts glyphicon-headphones a15-color-orange" aria-hidden="true"></span> Reach for Earth.mp3</li>
+			  <li class="list-group-item"><span class="glyphicon glyphicon-rts glyphicon-headphones a15-color-orange" aria-hidden="true"></span> With only Love.mp3</li>
+			</ul>
+		</div>
+		<div class="row">
+			<div class="opensans sub-title a15-color-orange">Cinema - 5 Tracks</div>
+		</div>
+		<div class="row">
+			<div class="paragraph-message">
+				<p class="opensans300 a15-color-black">
+					<img class="collection-img clear-img" src="<?php echo get_stylesheet_directory_uri(); ?>/img/cinema-package.png" alt="cinema music collection digital download" width="300" />Captivate your audience. They need to experience tension, fear, or empowerment. With these 5 tracks, your story will become alive and vivid.
+				</p>
+			</div>
+			<br>
+			<ul class="list-group">
+			  <li class="list-group-item"><span class="glyphicon glyphicon-rts glyphicon-headphones a15-color-orange" aria-hidden="true"></span> Armed Intel.mp3</li>
+			  <li class="list-group-item"><span class="glyphicon glyphicon-rts glyphicon-headphones a15-color-orange" aria-hidden="true"></span> Contact.mp3</li>
+			  <li class="list-group-item"><span class="glyphicon glyphicon-rts glyphicon-headphones a15-color-orange" aria-hidden="true"></span> Deploy.mp3</li>
+			  <li class="list-group-item"><span class="glyphicon glyphicon-rts glyphicon-headphones a15-color-orange" aria-hidden="true"></span> Desert Intro.mp3</li>
+			  <li class="list-group-item"><span class="glyphicon glyphicon-rts glyphicon-headphones a15-color-orange" aria-hidden="true"></span> Marksman.mp3</li>
+			</ul>
+		</div>
+		<div class="row">
+			<div class="opensans sub-title a15-color-orange">Filler - 5 Tracks</div>
+		</div>
+		<div class="row">
+			<div class="paragraph-message">
+				<p class="opensans300 a15-color-black">
+					<img class="collection-img clear-img" src="<?php echo get_stylesheet_directory_uri(); ?>/img/filler-package.png" alt="filler music collection digital download" width="300" />Keeping things simple is ideal. These 5 tracks allow your listener to focus on the essential part of your story while keeping them engaged with a little something in the background.
+				</p>
+			</div get_stylesheet_directory_uri()/img
+			<br>
+			<ul class="list-group">
+			  <li class="list-group-item"><span class="glyphicon glyphicon-rts glyphicon-headphones a15-color-orange" aria-hidden="true"></span> I am Him.mp3</li>
+			  <li class="list-group-item"><span class="glyphicon glyphicon-rts glyphicon-headphones a15-color-orange" aria-hidden="true"></span> I'll be Here.mp3</li>
+			  <li class="list-group-item"><span class="glyphicon glyphicon-rts glyphicon-headphones a15-color-orange" aria-hidden="true"></span> Life on Earth.mp3</li>
+			  <li class="list-group-item"><span class="glyphicon glyphicon-rts glyphicon-headphones a15-color-orange" aria-hidden="true"></span> My Old Shoes.mp3</li>
+			  <li class="list-group-item"><span class="glyphicon glyphicon-rts glyphicon-headphones a15-color-orange" aria-hidden="true"></span> Oceans.mp3</li>
+			</ul>
+		</div>
+		<hr>
+		<div class="row footer">
+			<a href="https://restorethesound.com/checkout/?add-to-cart=62"><div class="a15-button-center opensans300"><strong>DOWNLOAD</strong></div></a>
+		</div>
+		<!--<div class="row">
+			<div class="opensans300 main-title a15-color-blue">Do you only need 1 collection?</strong></div>
+		</div>
+		<div class="row">
+			<div class="paragraph-message">
+				<p class="opensans300 a15-color-black">
+					Here are some options! Each collection is $25. <a class="a15-color-orange" href="https://restorethesound.com/product-category/collections/">Click here.</a>
+				</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="opensans300 main-title a15-color-blue">Still Unsure? We can fix that.</div>
+			<div class="paragraph-message">
+				<p class="opensans300 a15-color-black">
+					We offer a 30-Day Money-Back Guarantee. If for any reason you are unsatisfied with our collection, we will promptly issue a refund. Simply email <a class="a15-color-orange" href="https://restorethesound.com/support/" target="_top">support</a> and we will contact you within 48 hours.
+				</p>
+			</div>
+		</div>
+		<div class="row footer">
+			<!--<div class="Free Download-song-msg opensans300 a15-color-blue"><strong>Not Ready? Tryout a Free Download song today!</strong>
+			<a href="https://restorethesound.com/product-category/Free Download-song/"><div class="Free Download-song-button a15-button opensans300">I <span class="glyphicon glyphicon-heart"></span> Free Download</div></a>
+				<!--<div class="opensans300 main-title main-title a15-color-blue"><strong>Not Ready? Tryout a Free Download song today!</strong></div>
+				<?php echo do_shortcode( '[contact-form-7 id="217" title="Contact form 1"]' ); ?>
+		</div>-->
+	</div>
+</body>
+</html>
